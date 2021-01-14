9be6ef2a588eb96f5a0207dd2ef72a9c21c0bf58
dilsharajakaruna <dilsharajakaruna@users.noreply.github.com>
2021-01-14 18:42:05 +0530

    add models

---
+++ Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Pharmacy.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+        public string Email { get; set; }
+        public int TeleNo { get; set; }
+    }
+}
